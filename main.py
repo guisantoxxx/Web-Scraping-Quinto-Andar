d2d528b3fec6bd2d5ebaaca8753fed676b70709e
JoaoMesa <navesmesajoao@gmail.com>
2025-02-19 10:43:33 -0300

    Colocando mais cidades

--- main.py
+++ main.py
@@ -8,8 +8,15 @@ links = ['https://www.quintoandar.com.br/comprar/imovel/belo-horizonte-mg-brasil
          'https://www.quintoandar.com.br/comprar/imovel/niteroi-rj-brasil', 'https://www.quintoandar.com.br/comprar/imovel/campinas-sp-brasil',
          'https://www.quintoandar.com.br/comprar/imovel/sao-bernardo-do-campo-sp-brasil', 'https://www.quintoandar.com.br/comprar/imovel/osasco-sp-brasil'
          , 'https://www.quintoandar.com.br/comprar/imovel/sao-goncalo-rj-brasil','https://www.quintoandar.com.br/comprar/imovel/duque-de-caxias-rj-brasil'
-         , 'https://www.quintoandar.com.br/comprar/imovel/belford-roxo-rj-brasil']
-estados = ['MG', 'SP', 'SP', 'RJ', 'RJ', 'SP', 'SP', 'SP', 'RJ', 'RJ', 'RJ']
+         , 'https://www.quintoandar.com.br/comprar/imovel/belford-roxo-rj-brasil',
+         'https://www.quintoandar.com.br/comprar/imovel/canoas-rs-brasil',
+         'https://www.quintoandar.com.br/comprar/imovel/porto-alegre-rs-brasil'
+         'https://www.quintoandar.com.br/comprar/imovel/jundiai-sp-brasil',
+         'https://www.quintoandar.com.br/comprar/imovel/novo-hamburgo-rs-brasil',
+         'https://www.quintoandar.com.br/comprar/imovel/sao-leopoldo-rs-brasil',
+         'https://www.quintoandar.com.br/comprar/imovel/contagem-mg-brasil'
+         ]
+    estados = ['MG', 'SP', 'SP', 'RJ', 'RJ', 'SP', 'SP', 'SP', 'RJ', 'RJ', 'RJ', 'RS', 'RS', 'SP', 'RS', 'RS', 'MG']
 
 for i in range(len(links)):
     executar_scraping(links[i], estados[i], 50)
